9e17ab02e4beb97d207360ea1bc237e1d2871a2a
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2019-10-04 21:21:25 +0300

    studied arrays, for loops, if statements

--- src/main/java/org/fasttrackit/racinggame/App.java
+++ src/main/java/org/fasttrackit/racinggame/App.java
@@ -3,58 +3,61 @@ package org.fasttrackit.racinggame;
 public class App {
 
     public static void main(String[] args) {
-        Engine engine = new Engine();
-        engine.manufacturer = "BavariaMotors";
-        engine.cubicCentimeters = 3000;
+        Game game = new Game();
+        game.start();
 
-        Car car = new Car(engine);
-        car.setName("BMW");
-        car.setColor("gray");
-        car.doorCount = 4;
-        car.setMaxSpeed(240);
-        car.setMileage(10.5);
-        car.setRunning(true);
-
-        System.out.println(car.toString());
-
-        double speed = 100;
-
-
-        double distance = car.accelerate(60, 1);
-
-
-        System.out.println(car);
-
-
-        System.out.println("Engine details: " +
-                car.engine.manufacturer);
-
-        engine.manufacturer = "VW";
-
-        System.out.println("Engine details2: " +
-                car.engine.manufacturer);
-
-        car.engine.manufacturer = "Renault";
-
-        System.out.println("Engine details3: " +
-                car.engine.manufacturer);
-
-        System.out.println("Engine: " + engine.manufacturer);
-
-
-        System.out.println("Car1: " + car.getName());
-
-        Car car2 = new Car(new Engine());
-        car2.setName("Kia");
-
-        System.out.println("Car2: " + car2.getName());
-
-        car2.setName("Dacia");
-        System.out.println("Car2: " + car2.getName());
-
-        System.out.println(car2.getColor());
-        System.out.println(car2.doorCount);
-        System.out.println(car2.isRunning());
+//        Engine engine = new Engine();
+//        engine.manufacturer = "BavariaMotors";
+//        engine.cubicCentimeters = 3000;
+//
+//        Car car = new Car(engine);
+//        car.setName("BMW");
+//        car.setColor("gray");
+//        car.doorCount = 4;
+//        car.setMaxSpeed(240);
+//        car.setMileage(10.5);
+//        car.setRunning(true);
+//
+//        System.out.println(car.toString());
+//
+//        double speed = 100;
+//
+//
+//        double distance = car.accelerate(60, 1);
+//
+//
+//        System.out.println(car);
+//
+//
+//        System.out.println("Engine details: " +
+//                car.engine.manufacturer);
+//
+//        engine.manufacturer = "VW";
+//
+//        System.out.println("Engine details2: " +
+//                car.engine.manufacturer);
+//
+//        car.engine.manufacturer = "Renault";
+//
+//        System.out.println("Engine details3: " +
+//                car.engine.manufacturer);
+//
+//        System.out.println("Engine: " + engine.manufacturer);
+//
+//
+//        System.out.println("Car1: " + car.getName());
+//
+//        Car car2 = new Car(new Engine());
+//        car2.setName("Kia");
+//
+//        System.out.println("Car2: " + car2.getName());
+//
+//        car2.setName("Dacia");
+//        System.out.println("Car2: " + car2.getName());
+//
+//        System.out.println(car2.getColor());
+//        System.out.println(car2.doorCount);
+//        System.out.println(car2.isRunning());
 
 
 //        car2 = car;
@@ -74,21 +77,21 @@ public class App {
 //        System.out.println("Value from car2: " + car2.totalVehicleCount);
 //        System.out.println("Value from Car class: " + Car.totalVehicleCount);
 
-        System.out.println("Total vehicle count: " + Vehicle.getTotalVehicleCount());
-
-
-        // method implementation taken from object type, not from variable type
-        Vehicle cheater = new Cheater();
-        cheater.accelerate(60, 1);
+//        System.out.println("Total vehicle count: " + Vehicle.getTotalVehicleCount());
+//
+//
+//        // method implementation taken from object type, not from variable type
+//        Vehicle cheater = new Cheater();
+//        cheater.accelerate(60, 1);
 
         // exposed methods taken from variable type, not from object type
 //        cheater.win();
 
         // type casting
-        ((Cheater) cheater).win();
-
-        Mechanic mechanic = new Mechanic();
-        mechanic.repair(cheater);
-        mechanic.repair(new Car(new Engine()));
+//        ((Cheater) cheater).win();
+//
+//        Mechanic mechanic = new Mechanic();
+//        mechanic.repair(cheater);
+//        mechanic.repair(new Car(new Engine()));
     }
 }
--- src/main/java/org/fasttrackit/racinggame/Track.java
+++ src/main/java/org/fasttrackit/racinggame/Track.java
@@ -2,6 +2,30 @@ package org.fasttrackit.racinggame;
 
 public class Track {
 
-    String name;
-    double length;
+    private String name;
+    private double length;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return "Track{" +
+                "name='" + name + '\'' +
+                ", length=" + length +
+                '}';
+    }
 }
